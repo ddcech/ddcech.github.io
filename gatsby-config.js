9ba7055a5dc727941e86ba2c33a6eba73700c118
David Cech <lilcech@gmail.com>
2019-01-30 17:14:09 +0100

    gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         path: `${__dirname}/src/pages`
       }
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects`
+      }
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
 
